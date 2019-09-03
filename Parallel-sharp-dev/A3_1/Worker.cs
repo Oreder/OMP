37c768bcd0e51e479e4764e71e24b60b3bf2fc80
Oreder <stepup.ored@gmail.com>
2019-09-04 02:33:17 +0300

    Multithreading tutorial: How do 2 threads work? How to send an object to thread using non-static method?

---
+++ Parallel-sharp-dev/A3_1/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace A3_1
+{
+    class Worker
+    {
+        private readonly string name;
+        private readonly int loop;
+
+        public Worker(string name, int loop)
+        {
+            this.name = name;
+            this.loop = loop;
+        }
+
+        public void DoWork(object value)
+        {
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine(name + " working " + value);
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
