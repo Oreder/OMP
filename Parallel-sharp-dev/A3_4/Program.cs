15f4194e170b7297ac488c9e99d8974c62613c5e
Oreder <stepup.ored@gmail.com>
2019-09-04 03:25:04 +0300

    Multithreading Tutorial: Naming threads

---
+++ Parallel-sharp-dev/A3_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace A3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+
+            var lego = new Thread(Lego)
+            {
+                Name = "LeGo"
+            };
+
+            lego.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " is running ...");
+                Thread.Sleep(20);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Lego(object obj)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " is running ...");
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
